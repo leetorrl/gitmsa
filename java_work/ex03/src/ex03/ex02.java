3f063c488b9f4abea0940d3e9f9418ef20b853d0
leetorrl <leetorrl123@gmail.com>
2024-07-05 16:25:02 +0900

    0705

--- java_work/ex03/src/ex03/ex02.java
+++ java_work/ex03/src/ex03/ex02.java
@@ -10,7 +10,7 @@ public class ex02 {
 	
 	public static void main(String[] args) {
 		
-		Scanner scan = new scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		//스케너는 무엇인가를 입력받아서 사용할때 쓰는 문법		
 		
 		int a;

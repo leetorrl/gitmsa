4936b10cc35c146077f935fa5c45f7d7458b1325
leetorrl <leetorrl123@gmail.com>
2024-07-08 16:30:32 +0900

    0708

--- java_work/Ex08/src/Ex05.java
+++ java_work/Ex08/src/Ex05.java
@@ -7,11 +7,13 @@ public class Ex05 {
 // ..
 //    재귀 함수= 자기자신의 함수를 호출하는것
 
-    public static int fact(int num){
+    public static int fact(int num) { //숫자의 반복문으로 생각하는게 편함
 
-        if (num > 0){
-        return num * fact(num-1);
-        // 5*fact(4)
+        if (num >0) {
+            return 2 * fact(num-1);
+
+
+            // 5*fact(4)
 //         5*4*fact(3)
 //            5*4*3*fact(2)
 //            5*4*3*2*fact(1)
@@ -23,13 +25,13 @@ public class Ex05 {
         }
     }
 
-    public static void main(String[] args) {
+        public static void main (String[]args){
 
-        int result= fact(5);
-        System.out.println(result);
+            int result = fact(4);
+            System.out.println(result);
 //        String[] strary = {"aa","bb","cc"};
 //        System.out.println("배열 선언함");
 //        main(strary);
-    }
+        }
 
-}
+    }

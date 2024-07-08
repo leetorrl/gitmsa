d0faf45c860c5a3008b683668a6a9fb84dd0b904
leetorrl <leetorrl123@gmail.com>
2024-07-08 10:33:54 +0900

    0708

---
+++ java_work/Ex08/src/Ex03.java
@@ -0,0 +1,14 @@
+package Ex08.src;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        int value = Math.random(1);
+
+        System.out.println("value="+value);
+
+
+
+    }
+
+}

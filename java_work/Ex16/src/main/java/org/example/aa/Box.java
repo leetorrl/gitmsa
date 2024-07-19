a9c5a90773287ee7e1d0707f1efcdaab54d817a0
leetorrl <leetorrl123@gmail.com>
2024-07-19 10:37:38 +0900

    0719

--- java_work/Ex16/src/main/java/org/example/aa/Box.java
+++ java_work/Ex16/src/main/java/org/example/aa/Box.java
@@ -1,15 +1,15 @@
-package org.example.aa;
-
-import lombok.AllArgsConstructor;
-import lombok.ToString;
-
-@AllArgsConstructor
-@ToString
-
-public class Box {
-
-    private String conts;
-
-
-
-}
+//package org.example.aa;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.ToString;
+//
+//@AllArgsConstructor
+//@ToString
+//
+//public class Box {
+//
+//    private String conts;
+//
+//
+//
+//}

d9070079fc75909d12f51ffc6ab512fdd92887a3
leetorrl <leetorrl123@gmail.com>
2024-09-02 10:45:05 +0900

    0902

--- java_work/Ex32/src/main/java/org/example/SKLL_library.java
+++ java_work/Ex32/src/main/java/org/example/SKLL_library.java
@@ -9,17 +9,26 @@ import java.time.LocalDateTime;
 @ToString
 @RequiredArgsConstructor
 @NoArgsConstructor
-@AllArgsConstructor
+
 
 
 public class SKLL_library {
-
     private int B_id;
     private String B_name;
     private String B_type;
     private String publisher;
     private int B_state;
     private LocalDateTime B_date;
+
+
+    public SKLL_library(int b_id, String b_name, String b_type, String publisher, int b_state, LocalDateTime b_date) {
+        this.B_id = b_id;
+        this.B_name = b_name;
+        this.B_type = b_type;
+        this.publisher = publisher;
+        this.B_state = b_state;
+        this.B_date = b_date;
+    }
 
 
 }

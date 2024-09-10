49cbb0fbcaf76555d71b288239a1106de477090d
leetorrl <leetorrl123@gmail.com>
2024-09-10 12:56:45 +0900

    0910

--- html_work/tailwind.config.js
+++ html_work/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
---
+++ vue_work/ex04/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-yellow-300">
+<h1>ABOUT</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

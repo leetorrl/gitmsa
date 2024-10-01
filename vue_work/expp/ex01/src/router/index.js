7219caf7384b3fc2a527d6b1f56a1ea89fd4ff9a
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-10-01 18:10:27 +0900

    1001

--- vue_work/expp/ex01/src/router/index.js
+++ vue_work/expp/ex01/src/router/index.js
@@ -6,6 +6,9 @@ import Child from '@/components/Child.vue'
 import Text2 from '@/components/Text2.vue'
 
 import Text3 from '@/components/Text3.vue'
+import Text4 from '@/components/Text4.vue';
+
+import HomeView from '@/components/view/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +37,16 @@ const router = createRouter({
       path: '/Text3/:list',
       name: 'text3',
       component: Text3
+    },
+    {
+      path: '/Text4',
+      name: 'Green MSA',
+      component: Text4
+    },
+    {
+      path: '/HomeView',
+      name: 'homeview',
+      component: HomeView
     },
   ]
 })

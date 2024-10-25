314d8a34acf0eb92bc29286fc50b08f503f2db25
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-10-25 13:45:38 +0900

    1025

--- vue_work/학사관리시스템/mainbase/main/src/App.vue
+++ vue_work/학사관리시스템/mainbase/main/src/App.vue
@@ -38,9 +38,9 @@
 import { RouterView } from 'vue-router'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { useBoardlistStore } from './stores/Boardlist'
+// import { useBoardlistStore } from './stores/Boardlist'
 // import { storeToRefs } from 'pinia'
-import { watchEffect } from 'vue'
+// import { watchEffect } from 'vue'
 
 import { useloginPiniaStore } from './stores/Boardlist'
 import { login } from './api/logapi'
@@ -48,14 +48,14 @@ import { login } from './api/logapi'
 import header from '@/layout/header.vue'
 import footer from '@/layout/footer.vue'
 
-import { onMounted } from 'vue'
+// import { onMounted } from 'vue'
 
 const headd = header
 const foott = footer
 
 const loginPinia = useloginPiniaStore()
 
-const Boardlist = useBoardlistStore()
+// const Boardlist = useBoardlistStore()
 
 // const { tsetidx } = storeToRefs(Boardlist)
 const router = useRouter()
@@ -83,13 +83,13 @@ const goManager = () => {
 }
 
 //토큰값 넣고 유저 권한용도로 쓸거 ex ADMIN , user
-const roles = ref('')
+// const roles = ref('')
 
 Mainhome()
 
-onMounted(() => {
-  loginPinia.pinialogin(token)
-})
+// onMounted(() => {
+//   loginPinia.pinialogin(token)
+// })
 
 const autologin = async () => {
   const userid = ref('userid3')
@@ -98,72 +98,73 @@ const autologin = async () => {
   console.log('아이디 = ' + userid.value)
   console.log('비밀번호 = ' + password.value)
 
-  const userdata = {
-    userid: userid.value,
-    password: password.value
+  // const userdata = {
+  //   userid: userid.value,
+  //   password: password.value
+  // }
+
+  //   try {
+  //     const apitoken = await login(userdata)
+
+  //     if (apitoken.status == 200) {
+  //       //status 리턴 안했으면 지우기
+
+  //       console.log('조금 성장한 청소년 토큰' + apitoken.data)
+
+  //       localStorage.setItem('token', apitoken.data)
+
+  //       //이걸 logapi.js로 만들어야함
+  //       const token = localStorage.getItem('token')
+
+  //       console.log('로컬 저장된 어른 토큰')
+  //       console.log(token)
+
+  //       loginPinia.pinialogin(token)
+
+  //       alert('로그인 완료')
+  //     } else {
+  //       loginPinia.pinialogout()
+  //       localStorage.removeItem('token')
+
+  //       alert('status not 200')
+  //     }
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+  // }
+
+  const logout = () => {
+    loginPinia.pinialogout()
+    localStorage.removeItem('token')
+
+    alert('토큰 쥬금 ㅠㅠ')
   }
 
-  try {
-    const apitoken = await login(userdata)
-
-    if (apitoken.status == 200) {
-      //status 리턴 안했으면 지우기
-
-      console.log('조금 성장한 청소년 토큰' + apitoken.data)
-
-      localStorage.setItem('token', apitoken.data)
-
-      //이걸 logapi.js로 만들어야함
-      const token = localStorage.getItem('token')
-
-      console.log('로컬 저장된 어른 토큰')
-      console.log(token)
-
-      loginPinia.pinialogin(token)
-
-      alert('로그인 완료')
+  const selecttoken = () => {
+    if (loginPinia.loginCheck) {
+      alert('로그인중')
     } else {
-      loginPinia.pinialogout()
-      localStorage.removeItem('token')
-
-      alert('status not 200')
+      alert('로그아웃되있음')
     }
-  } catch (e) {
-    console.log(e)
   }
+
+  // watchEffect(async () => {
+  //   const res = await loginCheck()
+
+  //   if (res == false) {
+  //     loginPinia.logout()
+  //   } else {
+  //     console.log('로그인 유지되는중 ' + res)
+
+  //     if (res.status !== 200) {
+  //       localStorage.removeItem('token')
+  //       loginPinia.logout()
+  //     } else {
+  //       loginPinia.login(res.data)
+  //     }
+  //   }
+  // })
 }
-
-const logout = () => {
-  loginPinia.pinialogout()
-  localStorage.removeItem('token')
-
-  alert('토큰 쥬금 ㅠㅠ')
-}
-
-const selecttoken = () => {
-  if (loginPinia.loginCheck) {
-    alert('로그인중')
-  } else {
-    alert('로그아웃되있음')
-  }
-}
-
-// watchEffect(async () => {
-//   const res = await loginCheck()
-
-//   if (res == false) {
-//     loginPinia.logout()
-//   } else {
-//     console.log('로그인 유지되는중 ' + res)
-
-//     if (res.status !== 200) {
-//       localStorage.removeItem('token')
-//       loginPinia.logout()
-//     } else {
-//       loginPinia.login(res.data)
-//     }
-//   }
-// })
 </script>
 
 <style lang="scss" scoped></style>

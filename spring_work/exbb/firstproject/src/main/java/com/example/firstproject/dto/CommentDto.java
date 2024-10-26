ce9e9193bcb8e0d171a2862fef368186db1c386a
leetorrl <leetorrl123@gmail.com>
2024-10-27 00:03:36 +0900

    1027

---
+++ spring_work/exbb/firstproject/src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -0,0 +1,35 @@
+package com.example.firstproject.dto;
+
+
+import com.example.firstproject.entity.Comment;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class CommentDto {
+
+    private Long id;
+
+    private Long articleId;
+
+    private String nickname;
+
+    private String body;
+
+
+
+
+    public static CommentDto createCommentDto(Comment comment) {
+
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
+}
+
--- spring_work/exbb/firstproject/src/main/resources/data.sql
+++ spring_work/exbb/firstproject/src/main/resources/data.sql
@@ -7,3 +7,19 @@
 insert into article( title, content) values( '가가가', '1111');
 insert into article( title, content) values( '나나나', '2222');
 insert into article( title, content) values( '다다다', '3333');
+
+insert into article( title, content) values('당신의 인생 영화는?' , '댓글 고');
+insert into article( title, content) values('당신의 소울 푸드는?' , '댓글 고고');
+insert into article( title, content) values('당신의 취미는?' , '댓글 고고고');
+
+insert into comment(article_id, nickname, body) values(4, 'Park', '굿 월 헌팅');
+insert into comment(article_id, nickname, body) values(4, 'Kim', '아이 엠 샘');
+insert into comment(article_id, nickname, body) values(4, 'Choi', '초밥');
+
+insert into comment(article_id, nickname, body) values(5, 'Park', '치킨');
+insert into comment(article_id, nickname, body) values(5, 'Kim', '샤브샤브');
+insert into comment(article_id, nickname, body) values(5, 'Choi', '초밥');
+
+insert into comment(article_id, nickname, body) values(6, 'Park', '조깅');
+insert into comment(article_id, nickname, body) values(6, 'Kim', '유튜브 시청');
+insert into comment(article_id, nickname, body) values(6, 'Choi', '독서');

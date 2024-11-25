9c7efee80a5477c9bca6139bf610c7f44a34de65
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-11-25 16:21:14 +0900

    11/25(월) node 추가 수업

--- msa_work/userservice/src/main/java/com/green/userservice/feign/FirstClient.java
+++ msa_work/userservice/src/main/java/com/green/userservice/feign/FirstClient.java
@@ -3,7 +3,7 @@ package com.green.userservice.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "first-service", url = "http://localhost:40001")
+@FeignClient(name = "first-service", url = "${first-service-url}")
 public interface FirstClient {
 
     @GetMapping("/first-service/test")
